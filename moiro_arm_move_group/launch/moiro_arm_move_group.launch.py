46250fce8be7c2c5ad1bd00e9203fe86f6de3f39
Mincheol710313 <mincheol710313@gmail.com>
2024-06-05 11:31:25 +0900

    migrating description from moiro_description to moiro_arm

---
+++ moiro_arm_move_group/launch/moiro_arm_move_group.launch.py
@@ -0,0 +1,110 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
+from ament_index_python.packages import get_package_share_directory
+from moveit_configs_utils import MoveItConfigsBuilder
+
+
+def generate_launch_description():
+    moveit_config = (
+        MoveItConfigsBuilder("moiro_arm", package_name="moiro_arm_moveit_config")
+        .robot_description(file_path="config/moiro_arm.urdf.xacro")
+        .trajectory_execution(file_path="config/moveit_controllers.yaml")
+        # only use ompl for planning
+        .planning_pipelines(pipelines=["ompl"])
+        .to_moveit_configs()
+    )
+    # Start the actual move_group node/action server
+    run_move_group_node = Node(
+        package="moveit_ros_move_group",
+        executable="move_group",
+        output="screen",
+        parameters=[
+            {"use_sim_time": True},
+            moveit_config.to_dict()],
+    )
+
+    # RViz
+    rviz_config_file = (
+        get_package_share_directory("moiro_arm_move_group") + "/launch/default_move_group.rviz"
+    )
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        arguments=["-d", rviz_config_file],
+        parameters=[
+            moveit_config.robot_description,
+            moveit_config.robot_description_semantic,
+            moveit_config.robot_description_kinematics,
+        ],
+    )
+
+    # Static TF
+    static_tf = Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        name="static_transform_publisher",
+        output="log",
+        arguments=["--frame-id", "world", "--child-frame-id", "base_link"],
+    )
+
+    # Publish the robot state
+    robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        output="both",
+        parameters=[
+            moveit_config.robot_description,
+        ],
+    )
+
+    # ros2_control using FakeSystem as hardware
+    ros2_controllers_path = os.path.join(
+        get_package_share_directory("moiro_arm_moveit_config"),
+        "config",
+        "ros2_controllers.yaml",
+    )
+    ros2_control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[ros2_controllers_path],
+        remappings=[
+            ("/controller_manager/robot_description", "/robot_description"),
+        ],
+        output="both",
+    )
+
+    # joint_state_publisher_gui_node = Node(
+    #     package='joint_state_publisher_gui',
+    #     executable='joint_state_publisher_gui',
+    #     name='joint_state_publisher_gui'
+    # )
+
+    # Load controllers
+    load_controllers = []
+    for controller in [
+        "moiro_arm_controller",
+        "joint_state_broadcaster",
+    ]:
+        load_controllers += [
+            ExecuteProcess(
+                cmd=["ros2 run controller_manager spawner {}".format(controller)],
+                shell=True,
+                output="screen",
+            )
+        ]
+
+    return LaunchDescription(
+        [
+            rviz_node,
+            static_tf,
+            robot_state_publisher,
+            run_move_group_node,
+            ros2_control_node,
+        ]
+        + load_controllers
+    )
---
+++ moiro_arm_move_group/launch/moiro_arm_move_group_service_class_interface.launch.py
@@ -0,0 +1,23 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from moveit_configs_utils import MoveItConfigsBuilder
+
+
+def generate_launch_description():
+    moveit_config = MoveItConfigsBuilder("moiro_arm", package_name="moiro_arm_moveit_config").to_moveit_configs()
+    use_sim_time = True
+    # MoveGroupInterface demo executable
+    move_group_demo = Node(
+        package="moiro_arm_move_group",
+        executable="moiro_arm_move_group_service_class_node",
+        output="screen",
+        parameters=[
+            {"use_sim_time": use_sim_time},
+            moveit_config.robot_description,
+            moveit_config.robot_description_semantic,
+            moveit_config.robot_description_kinematics,
+        ],
+
+    )
+
+    return LaunchDescription([move_group_demo])
